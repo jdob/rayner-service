f5ca2195cde9be1406ae35c534bcf243f6ddfb37
Jay Dobies <jason.dobies@redhat.com>
2018-09-17 16:32:54 -0400

    Added change logging

--- rayner/service/api.py
+++ rayner/service/api.py
@@ -1,7 +1,11 @@
+import socket
+
 from django.conf import settings
 import phue
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from rayner.service.models import ChangeEvent
 
 
 class LightAPI(APIView):
@@ -16,10 +20,34 @@ class LightAPI(APIView):
 
     def put(self, request):
         hue = request.data['hue']
+        service_ip = socket.gethostbyname(socket.gethostname())
+        client_ip = self.get_client_ip(request)
+        client_id = request.data.get('client_id', 'Unknown')
+
+        # Log the request if a database is present
+        if settings.DATABASE_FOUND:
+            ce = ChangeEvent(service_ip=service_ip,
+                             client_ip=client_ip,
+                             client_id=client_id,
+                             color=hue)
+            ce.save()
+
+        # Trigger the change; this will eventually need to be changed to
+        # a throttled queue for the load balancing test
         self.bridge().set_light(settings.BRIDGE_LIGHT, 'hue', hue)
+
         return Response()
 
     @staticmethod
     def bridge():
         return phue.Bridge(ip=settings.BRIDGE_IP,
                            username=settings.BRIDGE_TOKEN)
+
+    @staticmethod
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
