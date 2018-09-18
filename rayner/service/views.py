ab413fb3a0347e4cac0a971020414bd823aa65c1
Jay Dobies <jason.dobies@redhat.com>
2018-09-18 10:20:13 -0400

    Added rgb and hex options for specifying color

--- rayner/service/views.py
+++ rayner/service/views.py
@@ -5,6 +5,7 @@ import phue
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rgbxy import Converter
 
 from rayner.service.models import ChangeEvent
 from rayner.service.serializers import ChangeEventSerializer
@@ -21,22 +22,31 @@ class LightAPI(APIView):
         return Response()
 
     def put(self, request):
-        hue = request.data['hue']
         service_ip = socket.gethostbyname(socket.gethostname())
         client_ip = self.get_client_ip(request)
         client_id = request.data.get('client_id', 'Unknown')
+
+        # Figure out how the color is specified
+        c = Converter()
+        if 'rgb' in request.data:
+            r, g, b = request.data['rgb']
+            xy = c.rgb_to_xy(r, g, b)
+        elif 'hex' in request.data:
+            xy = c.hex_to_xy(request.data['hex'])
+
+        hex_color = c.xy_to_hex(*xy)
 
         # Log the request if a database is present
         if settings.DATABASE_FOUND:
             ce = ChangeEvent(service_ip=service_ip,
                              client_ip=client_ip,
                              client_id=client_id,
-                             color=hue)
+                             color=hex_color)
             ce.save()
 
         # Trigger the change; this will eventually need to be changed to
         # a throttled queue for the load balancing test
-        self.bridge().set_light(settings.BRIDGE_LIGHT, 'hue', hue)
+        self.bridge().set_light(settings.BRIDGE_LIGHT, 'xy', xy)
 
         return Response()
 
