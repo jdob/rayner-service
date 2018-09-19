8f433ef5749105700db49a961950a03eea51601a
Jay Dobies <jason.dobies@redhat.com>
2018-09-19 15:20:36 -0400

    Nevermind, that stats file crosses front and backend, so leave at the top

--- rayner/settings.py
+++ rayner/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'webpack-stats.dev.json'),
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.dev.json'),
     }
 }
 
